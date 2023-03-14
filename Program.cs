f91d387cae536b66033850912da3c2b480c24681
user <user@ZC415014>
2023-03-14 08:50:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace p117_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = ' ';
+
+            for (letter='a'; letter <= 'z'; letter++)
+            {
+                Console.Write(letter + " ");
+            }
+        }
+    }
+}
